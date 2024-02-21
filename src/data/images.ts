ac7773a7f980eef0cc44869b137f04841574dc0b
BastD29 <bastiendaniel9@gmail.com>
2024-02-21 12:41:07 +0100

    removed a tag underline & pasted assets in public folder

---
+++ src/data/images.ts
@@ -0,0 +1,14 @@
+import img1 from "../assets/images/photosnap.svg";
+import img2 from "../assets/images/eyecam-co.svg";
+import img3 from "../assets/images/insure.svg";
+import img4 from "../assets/images/loop-studios.svg";
+import img5 from "../assets/images/manage.svg";
+import img6 from "../assets/images/myhome.svg";
+import img7 from "../assets/images/the-air-filter-company.svg";
+import img8 from "../assets/images/shortly.svg";
+import img9 from "../assets/images/account.svg";
+import img10 from "../assets/images/faceit.svg";
+
+const images = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10];
+
+export { images };
